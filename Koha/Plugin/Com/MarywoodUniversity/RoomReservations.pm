bab997a868a378f8afea0752af3165098354cbcd
Tobias Engelke <tobias.engelke@lmscloud.de>
2021-09-03 15:08:13 +0200

    fix sql syntax

--- Koha/Plugin/Com/MarywoodUniversity/RoomReservations.pm
+++ Koha/Plugin/Com/MarywoodUniversity/RoomReservations.pm
@@ -117,12 +117,12 @@ sub install() {
               CONSTRAINT calendar_ibfk FOREIGN KEY (borrowernumber) REFERENCES borrowers(borrowernumber)
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;},
         qq{CREATE INDEX $bookings_index ON $bookings_table(borrowernumber, roomid);},
-        q{CREATE TABLE $openinghours_table (
+        qq{CREATE TABLE $openinghours_table (
               `openid` INT NOT NULL AUTO_INCREMENT,
-              `day` TINYINT,
-              `start` DATETIME NOT NULL, -- start date/time of opening hours
-              `end` DATETIME NOT NULL, -- end date/time of opening hours
-              PRIMARY KEY (openid),
+              `day` INT NOT NULL,
+              `start` TIME NOT NULL, -- start date/time of opening hours
+              `end` TIME NOT NULL, -- end date/time of opening hours
+              PRIMARY KEY (openid)
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;},
         qq{CREATE INDEX $openinghours_index ON $openinghours_table(openid);},
         qq{CREATE TABLE $equipment_table (
